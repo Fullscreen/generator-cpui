5b828eaa22dc5018629787373f2741f5bbd62c8d
Paul Sweeney <paul@8-bitdesign.com>
2014-06-13 19:16:56 -0700

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,77 @@
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+
+
+module.exports = yeoman.generators.Base.extend({
+  init: function() {
+    var config = this.dest.readJSON('bower.json').yoconfig
+      , self = this
+
+    // Figure out where to poop things on disk
+    this.paths = {
+      scripts: config.appPath+'/scripts',
+      styles: config.appPath+'/stylesheets',
+      specs: config.specPath
+    }
+
+    // And what module should we require?
+    this.rootModule = config.rootModule
+
+    this.sep = config.separators
+    this.ns = config.namespace
+
+    // Create a git-addable directory
+    this.makeKeep = function (path) {
+      this.mkdir(path)
+      this.copy('gitkeep', path+'/.gitkeep')
+    }
+
+    this.log(yosay("Let's create you a new module!"))
+  },
+
+
+  askFor: function() {
+    var done = this.async()
+      , self = this
+
+    this.prompt({
+      message: "What are we calling your new module?",
+      name: 'module',
+      required: true,
+      type: 'input'
+    }, function(resp) {
+      self.module = resp.module
+      done()
+    })
+  },
+
+  createStubFiles: function() {
+    this.makeKeep(this.paths.scripts +'/'+ this.module)
+    this.makeKeep(this.paths.specs +'/'+ this.module)
+    this.makeKeep(this.paths.styles +'/'+ this.module)
+    this.template('_index.coffee', this.paths.scripts +'/'+ this.module + '/index.coffee')
+  },
+
+  // Inject our `fs.module` entry into the dependencies
+  // for the main CPUI app
+  rewriteAppCoffee: function() {
+    var appCoffee = this.paths.scripts + '/app.coffee'
+      , contents = this.readFileAsString(appCoffee)
+      , self = this
+
+    function inject(str, section) {
+      var placeholder = self.sep.post+section
+        , newContent = str + "\n" + placeholder
+      return contents.replace(placeholder, newContent)
+    }
+
+    // Inject our module dependencies
+    contents = inject("#= require "+this.module, "Requiring our modules")
+    contents = inject("  '"+this.ns+this.module+"'", "Our modules")
+
+    this.dest.write(appCoffee, contents)
+  }
+});
