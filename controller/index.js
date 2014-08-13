461c33d7c48267e08cb650d0f61b2e6ef3fa7fbe
Brandon Aaskov <brandonaaskov@gmail.com>
2014-08-12 21:02:23 -0700

    Fixes bug with controller creation.

--- controller/index.js
+++ controller/index.js
@@ -2,7 +2,6 @@ var Base = require('../lib/module-base.js')
   , _ = require('underscore')
   , path = require('path')
   , self = undefined
-  , done = undefined
 
 module.exports = Base.extend({
   constructor: function() {
@@ -16,28 +15,32 @@ module.exports = Base.extend({
 
   init: function() {
     self = this
-    done = this.async()
 
     if (self.configExists()) self._create()
     else self.createConfig()
   },
 
   _create: function () {
-    self.say("Let's create you a new controller!")
-    self.getModule(done)
+    this.say("Let's create you a new controller!")
+    this.getModule(this.async())
 
-    if (self.options.controller) {
-      self._setController(self.options.controller)
+    this.ctrlFile = undefined
+    this.ctrlClass = undefined
+
+    if (this.options.controller) {
+      this._setController(this.options.controller)
     }
   },
 
-  _setController: function(name) {
+  _setController: function (name) {
     this.ctrlFile  = this._.dasherize(name)
     this.ctrlClass = this._.classify(name + ' ctrl')
   },
 
   getName: function() {
     if (this.ctrlClass) { return }
+
+    var done = this.async()
 
     this.ask("What's the name of your controller?", function(name) {
       self._setController(name)
@@ -53,7 +56,7 @@ module.exports = Base.extend({
     // Create model
     this.mkdir(ctrlPath)
     this.template('_controller.coffee', path.join(ctrlPath, this.ctrlFile+'.coffee'))
-
+    
     // Create test
     this.mkdir(specPath)
     this.template('_test.coffee', path.join(specPath, this.ctrlFile+'.coffee'))
